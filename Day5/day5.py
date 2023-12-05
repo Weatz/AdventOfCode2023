045586d855ffb9875fc589c79c857c7cacf2afb2
Jean <jean.durand@madboxgames.io>
2023-12-06 00:06:00 +0100

    Day 5 part 2

--- Day5/day5.py
+++ Day5/day5.py
@@ -15,7 +15,7 @@ def CreateConversion(group):
     conversionDict = {}
     for i in range(1, len(group)):
         values = [int(e) for e in re.findall(r"(\d+)", group[i])]
-        conversionDict[(values[1], values[1] + values[2])] = values[0] - values[1]
+        conversionDict[(values[1], values[1] + values[2] - 1)] = values[0] - values[1]
     return conversionDict
 
 def ApplyConversions(conversionDicts, seed):
@@ -41,6 +41,50 @@ def Part1(lines):
     
     print(min(locations))
 
+def ApplyConversionOnOneRange(interval, conversionDict):
+    for conversionRange in conversionDict:
+        if(conversionRange[0] <= interval[0] and conversionRange[1] >= interval[1]):
+            return [(interval[0] + conversionDict[conversionRange],interval[1] + conversionDict[conversionRange])]
+        elif(conversionRange[0] > interval[0] and conversionRange[0] <= interval[1] <= conversionRange[1]):
+            return [(conversionRange[0] + conversionDict[conversionRange],interval[1] + conversionDict[conversionRange])] + ApplyConversionOnOneRange((interval[0], conversionRange[0] - 1), conversionDict)
+        elif(conversionRange[0] <= interval[0] <= conversionRange[1] and conversionRange[1] < interval[1]):
+            return [(interval[0] + conversionDict[conversionRange],conversionRange[1] + conversionDict[conversionRange])] + ApplyConversionOnOneRange((conversionRange[1] + 1, interval[1]), conversionDict)
+        elif(conversionRange[0] > interval[0] and conversionRange[1] < interval[1]):
+            return [(conversionRange[0] + conversionDict[conversionRange],conversionRange[1] + conversionDict[conversionRange])] + ApplyConversionOnOneRange((interval[0], conversionRange[0] - 1), conversionDict) + ApplyConversionOnOneRange((conversionRange[0] + 1, interval[1]), conversionDict)
+    return [interval]
+
+def ApplyRangeConversions(seedRange, conversionDicts):
+    intervals = [seedRange]
+    for conversionDict in conversionDicts:
+        #print("interval state : ", intervals)
+        tmpResultRanges = []
+        for interval in intervals:
+            tmpResultRanges = tmpResultRanges + ApplyConversionOnOneRange(interval, conversionDict)
+        intervals = tmpResultRanges
+    return intervals
+
+
+def Part2(lines):
+    splittedData = SplitData(lines)
+    conversionDicts = []
+    for i in range(1, len(splittedData)):
+        conversionDicts.append(CreateConversion(splittedData[i]))
+
+    splitSeeds = [int(e) for e in re.findall(r"(\d+)", splittedData[0][0])]
+    seedRanges = []
+    for i in range(0, int(len(splitSeeds) / 2)):
+        seedRanges.append((splitSeeds[2 * i], splitSeeds[2 * i] + splitSeeds[2*i +1] - 1))
+
+    resultRanges = []
+    for seedRange in seedRanges:
+        resultRanges = resultRanges + ApplyRangeConversions(seedRange, conversionDicts)
+
+    for resultRange in resultRanges:
+        print(resultRange)
+    mintuple = min(resultRanges)
+    print("minLocation : " + str(mintuple[0]))
+
+
 f = open("Day5\\data5.txt", "r")
 lines = f.readlines()
-Part1(lines)
+Part2(lines)
