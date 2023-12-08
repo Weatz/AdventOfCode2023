daeb95823fd4d36dd75773912e30ff49fdb441cd
Jean <jean.durand@madboxgames.io>
2023-12-08 17:04:54 +0100

    Day 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,14 +1,36 @@
 import re
 
 def ParseInput(lines):
+    l = lines
     nodeDict = {}
-    sequence = lines[0]
-    lines.pop(0)
-    lines.pop(0)
-    for line in lines:
+    sequence = l[0]
+    l.pop(0)
+    l.pop(0)
+    for line in l:
         split = re.findall(r"(\w+)", line)
         nodeDict[split[0]] = (split[1], split[2])
     return sequence, nodeDict
+
+
+def ParseInput2(lines):
+    print(len(lines))
+    l = lines
+    nodeDict = {}
+    sequence = lines[0]
+    startingNodes = []
+    finishNodes = []
+    l.pop(0)
+    l.pop(0)
+    for line in l:
+        split = re.findall(r"(\w+)", line)
+        nodeDict[split[0]] = (split[1], split[2])
+        if(split[0][2] == "A"):
+            startingNodes.append(split[0])
+        elif(split[0][2] == "Z"):
+            finishNodes.append("Z")
+    return sequence, nodeDict, startingNodes, finishNodes
+
+
 
 def ConvertSeq(sequence):
     convertedSeq = []
@@ -29,7 +51,26 @@ def Part1(lines):
         e += 1
     print(e)
 
+def Part2(lines):
+    seq, nodeDict, startingNodes, finishNodes = ParseInput2(lines)
+    convertedSeq = ConvertSeq(seq)
+    stepsToFinish = []
+    prod = len(convertedSeq)
+    print(startingNodes)
+    for start in startingNodes:
+        e = 0
+        currentNode = start
+        print(currentNode)
+        while (currentNode[2] != "Z"):
+            currentNode = (nodeDict[currentNode])[convertedSeq[e % len(convertedSeq)]]
+            e += 1
+        prod *= e / len(convertedSeq) 
+    print(prod)
+
+
 
 f = open("Day8/data8.txt", "r")
 lines = f.readlines()
-Part1(lines)
+#Part1(lines)
+print("-----")
+Part2(lines)
